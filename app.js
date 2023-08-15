736df25da51312cfd1f6874df7836615d61b09f7
Neeraj Kashyap <krneeraj1997@gmail.com>
2023-08-15 20:35:52 +0530

    blog post app created server is running on port 3000

---
+++ app.js
@@ -0,0 +1,48 @@
+/* The code is importing three modules in JavaScript: `express`, `ejs`, and `body-parser`. */
+import express from "express";
+import ejs from "ejs";
+import bodyParser from "body-parser";
+
+
+
+/* `const app=express();` creates an instance of the Express application. This instance is stored in
+the constant variable `app`, which is used to configure and run the server. */
+const app=express();
+const port=3000;
+
+
+/* `app.set('view engine','ejs');` sets the view engine for the Express application to EJS. This means
+that when rendering views, Express will use the EJS templating engine to generate HTML. */
+app.set('view engine','ejs');
+/* `app.use(express.static("./public"))` is a middleware function in Express.js that serves static
+files from the specified directory. In this case, it is serving static files from the "public"
+directory. This means that any files in the "public" directory, such as CSS files or images, can be
+accessed by the client. */
+app.use(express.static("./public"));
+/* `app.use(bodyParser.urlencoded({extended:true}));` is a middleware function in Express.js that
+parses incoming requests with URL-encoded payloads. */
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+
+
+
+
+
+
+
+
+
+/* `app.listen(port,()=>{...})` is a method in Express.js that starts a server and listens for incoming
+requests on the specified port. In this case, it is listening on port 3000. When the server starts
+running, it will log a message to the console saying "app is running on 3000". */
+app.listen(port,()=>{
+    console.log(`app is running on ${port}`);
+})
+
+
